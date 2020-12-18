77a901033353b8ca69505e8f8964f7f3fb9e697f
李满满 <limanman@corp.netease.com>
2020-12-18 15:34:44 +0800

    :underage:

--- namekox_zookeeper/core/allotter.py
+++ namekox_zookeeper/core/allotter.py
@@ -4,14 +4,19 @@
 
 
 from itertools import cycle
+from namekox_zookeeper.exceptions import RegServiceNotFound
 
 
 class Allotter(object):
+    def _raise(self, exc, errs=None):
+        raise
+
     def __init__(self, sdepd=None):
         self.iters = {}
         self.sdepd = sdepd
 
     def get(self, name):
+        name not in self.sdepd.services and self._raise(RegServiceNotFound, name)
         data = self.sdepd.services[name]
         self.iters.setdefault(name, cycle(data))
         return self.iters[name].next()
---
+++ namekox_zookeeper/exceptions.py
@@ -0,0 +1,7 @@
+#! -*- coding: utf-8 -*-
+
+# author: forcemain@163.com
+
+
+class RegServiceNotFound(Exception):
+    pass
