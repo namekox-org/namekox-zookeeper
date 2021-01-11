89fc75b4e8d8ca9092cd675e827dca626fed844f
李满满 <limanman@corp.netease.com>
2021-01-11 14:03:49 +0800

    :underage:

--- namekox_zookeeper/core/dependencies/__init__.py
+++ namekox_zookeeper/core/dependencies/__init__.py
@@ -3,19 +3,18 @@
 # author: forcemain@163.com
 
 
-import sys
 import six
 import json
 import socket
 
 
-from kazoo.retry import KazooRetry
 from kazoo.client import KazooClient
 from kazoo.protocol.states import KazooState
 from namekox_core.core.friendly import AsLazyProperty
 from namekox_core.core.generator import generator_uuid
 from namekox_core.core.friendly import ignore_exception
 from namekox_core.core.service.dependency import Dependency
+from kazoo.handlers.eventlet import SequentialEventletHandler
 from namekox_zookeeper.constants import ZOOKEEPER_CONFIG_KEY, DEFAULT_ZOOKEEPER_SESSION_TIMEOUT
 
 
@@ -25,7 +24,6 @@ class ZooKeeperHelper(Dependency):
         self.services = {}
         self.instance = None
         self.dbname = dbname
-        self.prestate = None
         self.watching = watching
         self.allotter = allotter
         self.coptions = coptions or {}
@@ -48,9 +46,9 @@ class ZooKeeperHelper(Dependency):
     def gen_serv_name(self, name):
         return '{}/{}.{}'.format(self.watching, name, self.serverid)
 
-    def update_zookeeper_services(self, c):
+    def update_zookeeper_services(self, o):
         services = {}
-        for name in c:
+        for name in o.get():
             path = '{}/{}'.format(self.watching, name)
             data = ignore_exception(json.loads)(self.instance.get(path)[0])
             name = self.get_serv_name(name)
@@ -59,38 +57,37 @@ class ZooKeeperHelper(Dependency):
         self.services = services
         self.allotter and self.allotter.set(self)
 
-    def setup_watching(self):
-        self.update_zookeeper_services = self.instance.ChildrenWatch(self.watching)(self.update_zookeeper_services)
+    def fetch_children(self):
+        self.instance.get_children_async(self.watching).rawlink(self.update_zookeeper_services)
 
     def setup_register(self):
         r_options = self.roptions.copy()
         host_addr = self.get_host_byname()
+
         r_options.setdefault('port', 80)
         r_options.setdefault('address', host_addr or '127.0.0.1')
-        self.instance.ensure_path(self.watching)
-        base_path = self.gen_serv_name(self.container.service_cls.name)
+
         host_info = json.dumps(r_options)
-        self.instance.create(base_path, host_info, ephemeral=True)
+        base_path = self.gen_serv_name(self.container.service_cls.name)
+        self.instance.create_async(base_path, host_info, ephemeral=True, makepath=True)
 
     def setup_listener(self, state):
-        if self.prestate == KazooState.LOST and state == KazooState.CONNECTED:
-            self.instance = KazooClient(**self.coptions)
-            self.instance.start()
-            self.setup_register()
-        self.prestate = state
+        state not in (KazooState.LOST, KazooState.SUSPENDED) and self.setup_register()
+        state not in (KazooState.LOST, KazooState.SUSPENDED) and self.fetch_children()
 
     def setup(self):
         config = self.configs.get(self.dbname, {}).copy()
+        config.setdefault('handler', SequentialEventletHandler())
         config.setdefault('timeout', DEFAULT_ZOOKEEPER_SESSION_TIMEOUT)
-        config.setdefault('connection_retry', KazooRetry(max_tries=sys.maxsize))
         [config.update({k: v}) for k, v in six.iteritems(self.coptions)]
+
         self.instance = KazooClient(**config)
         self.instance.add_listener(self.setup_listener)
-        self.instance.start()
+
         self.coptions = config
 
     def start(self):
-        self.watching and self.setup_watching()
+        self.instance and self.instance.start_async().wait(timeout=15)
         self.watching and self.setup_register()
 
     def stop(self):
